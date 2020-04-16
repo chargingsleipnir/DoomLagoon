6db59586df0602e3f76e7449347c62626854aa14
Odin von Doom <chargingsleipnir@hotmail.com>
2020-04-16 14:09:53 -0400

    Removed localhost references.

--- app.js
+++ app.js
@@ -9,64 +9,23 @@ var objHdlrs = require('./Server/ObjectHandlers.js')(dbHdlr);
 
 // VARIABLES -----
 
-const ip = '127.0.0.1',
-      port = 8000;
+const ip = process.env.IP || '127.0.0.1',
+      port = process.env.PORT || 8000;
 
-
-// CACHE -----
-
-//  Local cache for static content.
-var cache = { 'index': fs.readFileSync('./Client/index.html') };
-var cache_get = function (key) {
-    return cache[key];
-};
-
-
-// TERMINATION -----
-
-var terminator = function(sig) {
-  if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating sample app ...', Date(Date.now()), sig);
-      process.exit(1);
-  }
-  console.log('%s: Node server stopped.', Date(Date.now()) );
-};
-
-//  Process on exit and signals.
-process.on('exit', function () {
-  terminator();
-});
-
-// Removed 'SIGPIPE' from the list - bugz 852598.
-['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-  'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-].forEach(function(element, index, array) {
-  process.on(element, function () {
-      terminator(element);
-  });
-});
 
 // ROUTES -----
-
-var routes = { };
-
-routes['/asciimo'] = function(req, res) {
-    var link = "http://i.imgur.com/kmbjB.png";
-    res.send("<html><body><img src='" + link + "'></body></html>");
-};
-
-routes['/'] = function(req, res) {
-    //res.setHeader('Content-Type', 'text/html');
-    //res.send(cache_get('index'));
-    //replace above two lines with this?? 
-    res.sendFile(__dirname + '/Client/index.html');
-};
 
 // This allows client access to everything under these directories.
 app.use(express.static(__dirname + '/Client'));
 app.use(express.static(__dirname + '/Shared'));
-
 app.use(express.static(__dirname + '/node_modules/phaser/dist/phaser.js'));
+
+var routes = { };
+
+routes['/'] = function(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile('./index.html');
+};
 
 for (var r in routes) {
   app.get(r, routes[r]);
