608658f34a82640b3d60d4d1a6eaaf937529fbfe
Odin von Doom <chargingsleipnir@hotmail.com>
2020-04-16 13:58:16 -0400

    just trying again

--- app.js
+++ app.js
@@ -56,10 +56,10 @@ routes['/asciimo'] = function(req, res) {
 };
 
 routes['/'] = function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(cache_get('index'));
+    //res.setHeader('Content-Type', 'text/html');
+    //res.send(cache_get('index'));
     //replace above two lines with this?? 
-    //res.sendFile(__dirname + '/Client/index.html');
+    res.sendFile(__dirname + '/Client/index.html');
 };
 
 // This allows client access to everything under these directories.
